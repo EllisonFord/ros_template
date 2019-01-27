ea7fe8f5bd8b573d7a593aee25d936a2a63f8963
Sam <sam@bam.com>
2019-01-27 19:30:00 +0100

    Reading from the yaml in python

--- scripts/py_node.py
+++ scripts/py_node.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python2
 
+import yaml
 import rospy
 from std_msgs.msg import String
 
 
 if __name__ == '__main__':
-    pass
+
+    with open("../config/params.yaml", 'r') as stream:
+        try:
+            params_content = yaml.load(stream)
+
+            # Getting all of the params content
+            rgb_topic = params_content["rgb_topic"]
+            results_topic = params_content["results_topic"]
+            namespace = params_content["namespace"]
+
+        except yaml.YAMLError as exc:
+            print(exc)
